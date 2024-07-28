4da44a1dd59a29bf314465464f8dd06e59cf614e
Rodrigo Guggiana <rnguggiana@gmail.com>
2024-07-28 12:51:44 -0300

    clase1

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,14 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	/*Declaracion de una variable, palabra reservada para obtener dato del usuario
+	se utiliza let nombre*/
+
+	let nombre;
+	
+	nombre = prompt("Ingrese su nombre"); 
+
+	alert("Su nombre es: " + nombre);
+	
 }
 
--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -3,10 +3,13 @@ Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
-
+	// Tomar el valor del campo de texto por ID, declaro la variable dato
+	let dato;
+	
+	dato = document.getElementById("nombre").value; //le asigno a la variable el valor del getelement
+            
+	// Mostrar el dato con alert
+	alert("El dato ingresado es: " + dato);
 }
 
 
